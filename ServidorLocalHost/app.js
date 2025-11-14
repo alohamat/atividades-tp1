bfaa904af7e1d345452f017bb8de473aeafa2fbd
alohamat <matalohazk@gmail.com>
2025-11-14 14:41:48 -0300

    Um servidor acessado via localhost atividade

---
+++ ServidorLocalHost/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/sobre', (req, res) => {
+  res.render('sobre');
+});
+
+app.get('/contato', (req, res) => {
+  res.render('contato');
+});
+
+app.listen(port, () => {
+  console.log("Servidor rodando na porta ", port);
+});
