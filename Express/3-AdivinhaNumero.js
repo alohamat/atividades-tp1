bd0f025f62b8c1104f57885d51c6539ad9b7d7ae
Kayky Cruz <164812936+alohamat@users.noreply.github.com>
2025-12-05 14:31:41 -0300

    Refactor guessing game to support multiple players

--- Express/3-AdivinhaNumero.js
+++ Express/3-AdivinhaNumero.js
@@ -2,38 +2,51 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.use(express.urlencoded({ extended: true }));
 
-const numeroSecreto = Math.floor(Math.random() * 100) + 1;
-console.log("Número secreto:", numeroSecreto);
+app.use(express.urlencoded({ extended: true }));
+const dicio = {};
+
 
 app.get("/", (req, res) => {
   res.send(`
-    <h1>Adivinhe o Número</h1>
-    <form method="POST">
-      <input type="number" name="palpite" required>
-      <button type="submit">Enviar</button>
-    </form>
+  <h1>Adivinhe o Número</h1>
+  <form method="POST">
+  <input type="number" name="palpite" required>
+  <input type="text" name="id" required>
+  <button type="submit">Enviar</button>
+  </form>
   `);
 });
 
 app.post("/", (req, res) => {
-  const palpite = Number(req.body.palpite);
-  let msg = "";
-
-  if (palpite < numeroSecreto) msg = `Seu palpite (${palpite}) é muito baixo!`;
-  else if (palpite > numeroSecreto) msg = `Seu palpite (${palpite}) é muito alto!`;
-  else msg = `ACERTOU MISERÁVEL! Era ${numeroSecreto}!`;
-
-  res.send(`
-    <h1>Adivinhe o Número</h1>
-    <p>${msg}</p>
-    <form method="POST">
-      <input type="number" name="palpite" required>
-      <button type="submit">Tentar de novo</button>
-    </form>
-  `);
-});
+    const palpite = Number(req.body.palpite);
+    const id = req.body.id;
+    let msg = "";
+  
+    if (!dicio[id]) {
+      dicio[id] = Math.floor(Math.random() * 100) + 1;
+      console.log(`Criado numero secreto para ${id}:`, dicio[id]);
+    }
+  
+    if (palpite < dicio[id]) msg = `${id}, palpite (${palpite}) é muito baixo!`;
+    else if (palpite > dicio[id]) msg = `${id}, palpite (${palpite}) é muito alto!`;
+    else {
+      msg = `ACERTOU, ${id}! Era ${dicio[id]}!`;
+      delete dicio[id]; // reseta o jogo
+    }
+  
+    res.send(`
+      <h1>Adivinhe o Número</h1>
+      <p>${msg}</p>
+      <form method="POST">
+        <input type="number" name="palpite" required>
+        <input type="hidden" name="id" value="${id}">
+        <button type="submit">Tentar de novo</button>
+      </form>
+    `);
+  });
+  
+  
 
 app.listen(port, () => {
     console.log(`Rodando em ${port}`);
